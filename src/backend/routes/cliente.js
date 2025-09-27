acfd39a46a232118f73882c12dd899fd882f5c57
pedrolucasods <ploliveira1805@gmail.com>
2025-09-27 16:41:15 -0400

    Rota cliente onde ficam as funcionalidades da opção cliente.

---
+++ src/backend/routes/cliente.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const clientemodel = require('../models/cliente')
+
+router.get('/',function(req,res){
+    clientemodel.findAll().then(function(clients){
+        res.render('cliente',{stylesheet:'stylecliente', script:'scriptcliente.js', layout:'main', clientes:clients})
+    })
+    
+})
+
+module.exports = router
