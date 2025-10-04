d69d96d12023c5babb00aeb9056223af8e2e5ed0
pedrolucasods <ploliveira1805@gmail.com>
2025-10-04 18:32:40 -0400

    Adicionei a validação do nome e uma mascara para tentar evitar que nomes inválidos sejam digitados e cadastrados no banco de dados.

--- src/frontend/public/javascript/addcliente.js
+++ src/frontend/public/javascript/addcliente.js
@@ -1,4 +1,53 @@
 document.addEventListener('DOMContentLoaded',function(){
+    mascara()
+
+    const form = document.getElementById('formaddcliente')
+    const spanname = document.getElementById('spanname')
+
+    form.addEventListener('submit',function(event){
+    let inputname = document.getElementById('inputnome').value 
+    if(!validatename(inputname)){
+        spanname.innerHTML = 'Erro, nome inválido!'
+        event.preventDefault()
+        }
+    })
+
+})
+
+// Validações
+
+function validatename(input){
+    if(input.trim() === '' || input.length <3){
+        return false
+    }
+    return true
+}
+
+
+// Mascaras
+function mascara(){
+
+    //Mascara campo nome
+
+    const inputname = document.getElementById('inputnome')
+    inputname.addEventListener('input', function () {
+        // Remove tudo que não for letra ou espaço
+        this.value = this.value.replace(/[^a-zA-ZÀ-ÿ\s]/g, '');
+
+        // Substitui múltiplos espaços por apenas um
+        this.value = this.value.replace(/\s{2,}/g, ' ');
+
+        // Remove espaço no início
+        this.value = this.value.replace(/^\s+/, '');
+
+    })
+
+    inputname.addEventListener('blur', function () {
+        this.value = this.value.replace(/\s+$/, ''); // remove espaços no final
+    });
+    
+    //Mascara campo telefone
+
     const telefoneinput = document.getElementById('telefone')
     if(telefoneinput){
         IMask(telefoneinput,{
@@ -6,10 +55,12 @@ document.addEventListener('DOMContentLoaded',function(){
         })
     }
 
+    //Mascara campo cpf
+
     const cpfinput = document.getElementById('cpf')
     if(cpfinput){
         IMask(cpfinput,{
             mask:'000.000.000-00'
         })
     }
-})
+}
