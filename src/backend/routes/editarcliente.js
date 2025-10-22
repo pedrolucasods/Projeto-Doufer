9a31d61b2499f1a47d1121c5298d8d83f2565e25
pedrolucasods <ploliveira1805@gmail.com>
2025-10-22 12:18:32 -0400

    se o cpf for 0 ele não retorna o cpf no input do formulario editar, se não for ele retorna o cpf no formulario

--- src/backend/routes/editarcliente.js
+++ src/backend/routes/editarcliente.js
@@ -5,8 +5,14 @@ router.get('/editar/:id/:nome/:telefone/:cpf',function(req,res){
     let idparam = req.params.id
     let nomeparam = req.params.nome
     let telefoneparam = req.params.telefone
-    let cpfparam = req.params.cpf
-    res.render('editarcliente',{script:'editarcliente.js',idparam,nomeparam,telefoneparam,cpfparam})
+    let cpfrequi = req.params.cpf
+    if(cpfrequi === '0'){
+        let cpfparam = null
+        res.render('editarcliente',{script:'editarcliente.js',idparam,nomeparam,telefoneparam,cpfparam})
+    }else{
+        let cpfparam = req.params.cpf
+        res.render('editarcliente',{script:'editarcliente.js',idparam,nomeparam,telefoneparam,cpfparam})
+    }
 })
 
 module.exports = router
