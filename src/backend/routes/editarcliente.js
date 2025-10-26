608643cab574c2cee251585faa6f362b0c71c15b
pedrolucasods <ploliveira1805@gmail.com>
2025-10-26 12:49:24 -0400

    Verifica agora também o nome da empresa na hora de fazer a alteração se o nome da empresa for igual a 0 a variavel com o parametro nome da empresa é igual a NULL, se não a variavel com o parametro nome da empresa é igual ao nome da empresa que esta vindo pelo url!

--- src/backend/routes/editarcliente.js
+++ src/backend/routes/editarcliente.js
@@ -1,17 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/editar/:id/:nome/:telefone/:cpf',function(req,res){
+router.get('/editar/:id/:nome/:telefone/:cpf/:nome_empresa',function(req,res){
     let idparam = req.params.id
     let nomeparam = req.params.nome
     let telefoneparam = req.params.telefone
     let cpfrequi = req.params.cpf
+    let nomeEmpresa_requi = req.params.nome_empresa
     if(cpfrequi === '0'){
         let cpfparam = null
-        res.render('editarcliente',{script:'editarcliente.js',idparam,nomeparam,telefoneparam,cpfparam})
+        if(nomeEmpresa_requi === '0'){
+            let nomeempresaParam = null
+            res.render('editarcliente',{script:'editarcliente.js',idparam,nomeparam,telefoneparam,cpfparam,nomeempresaParam})
+        }else{
+            let nomeempresaParam = nomeEmpresa_requi
+            res.render('editarcliente',{script:'editarcliente.js',idparam,nomeparam,telefoneparam,cpfparam,nomeempresaParam})
+        }
+        
     }else{
         let cpfparam = req.params.cpf
-        res.render('editarcliente',{script:'editarcliente.js',idparam,nomeparam,telefoneparam,cpfparam})
+        if(nomeEmpresa_requi === '0'){
+            let nomeempresaParam = null
+            res.render('editarcliente',{script:'editarcliente.js',idparam,nomeparam,telefoneparam,cpfparam,nomeempresaParam})
+        }else{
+            let nomeempresaParam = nomeEmpresa_requi
+            res.render('editarcliente',{script:'editarcliente.js',idparam,nomeparam,telefoneparam,cpfparam,nomeempresaParam})
+        }
     }
 })
 
