96c7af6098b529444e29a4fd00d2040f57c661d8
pedrolucasods <ploliveira1805@gmail.com>
2025-10-29 10:37:07 -0400

    Coloquei a função de mudar de rota no botão pedido do home. quando apertar ele leva para a rota pedidos. - Peguei o botão pelo id - Atribui a função nele

--- src/frontend/public/javascript/viewhome.js
+++ src/frontend/public/javascript/viewhome.js
@@ -1,9 +1,15 @@
 const btncliente = document.getElementById('btncliente')
-
+const btnpedido = document.getElementById('btnpedido')
 
 btncliente.addEventListener('click',function(){
     window.location.href = '/clientes'
 })
+
+btnpedido.addEventListener('click',function(){
+    window.location.href = '/pedidos'
+})
+
+
 
 const { remote } = require('electron');
 const win = remote.getCurrentWindow();
