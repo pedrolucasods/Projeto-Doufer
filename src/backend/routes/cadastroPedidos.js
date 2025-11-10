dec34499b36a7a08d19ba3e20d49616a244b9a13
pedrolucasods <ploliveira1805@gmail.com>
2025-11-10 12:08:00 -0400

    arquivo contento a rota onde recebe os dados do formulario pedido, ainda falta cadastrar no banco de  dados!

---
+++ src/backend/routes/cadastroPedidos.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const Pedido = require('../models/pedidos')
+const ItensPedido = require('../models/itensPedidos')
+
+router.post('/', function(req,res){
+  try {
+    if (req.body.pedido) {
+        let pedido = []
+        pedidojson = JSON.parse(req.body.pedido)
+        pedido.push(pedidojson);
+        return res.send(pedido)
+    }
+    
+  } catch (err) {
+    return res.status(400).send('Erro ao converter itens: ' + err.message);
+  }
+})
+
+module.exports = router
