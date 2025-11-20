74cad57e1daf9f3fc9268571f3db9984f19a16d1
pedrolucasods <ploliveira1805@gmail.com>
2025-11-20 16:37:11 -0400

    feat: configura motor Handlebars e adiciona helpers personalizados

--- src/backend/server.js
+++ src/backend/server.js
@@ -19,60 +19,71 @@ const association = require('./associations/associations')
 
 
 //Body Parser
-  app.use(bodyParser.urlencoded({extended: true}))
-  app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 //config
-  // Configura Handlebars, public, etc.
-  app.engine('handlebars', handlebars.engine({defaultLayout: 'main', 
-    runtimeOptions: {
-      allowProtoPropertiesByDefault: true,
+// Configura Handlebars, public, etc.
+app.engine('handlebars', handlebars.engine({
+  defaultLayout: 'main', 
+  helpers: {  // adicionado helpers para eq e outros
+    eq: (a, b) => a == b,
+    ne: (a, b) => a != b,
+    gt: (a, b) => a > b,
+    lt: (a, b) => a < b,
+    gte: (a, b) => a >= b,
+    lte: (a, b) => a <= b,
+    and: (a, b) => a && b,
+    or: (a, b) => a || b
+  },
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
       allowProtoMethodsByDefault: true,}}))
 
-  app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars')
 
 
-  app.set('views', path.join(__dirname,'../frontend/views'))
+app.set('views', path.join(__dirname,'../frontend/views'))
   
-  app.use(express.static(path.join(__dirname,'../frontend/public')))
+app.use(express.static(path.join(__dirname,'../frontend/public')))
 
 
 
-  
+
 // Rotas
 
-  // home
-  const homeroute = require('./routes/home')
-  app.use('/',homeroute)
+// home
+const homeroute = require('./routes/home')
+app.use('/',homeroute)
 
-  // Rotas dos clientes
-  const clienteroute = require('./routes/cliente')
-  app.use('/clientes',clienteroute)
+// Rotas dos clientes
+const clienteroute = require('./routes/cliente')
+app.use('/clientes',clienteroute)
 
-  const cadformcliente = require('./routes/addcliente')
-  app.use('/clientes/newcliente',cadformcliente)
+const cadformcliente = require('./routes/addcliente')
+app.use('/clientes/newcliente',cadformcliente)
 
-  const cadastrocliente = require('./routes/cadcliente')
-  app.use('/clientes/newcliente/cadcliente',cadastrocliente)
+const cadastrocliente = require('./routes/cadcliente')
+app.use('/clientes/newcliente/cadcliente',cadastrocliente)
 
-  const deletarcliente = require('./routes/deletarcliente')
-  app.use('/clientes',deletarcliente)
+const deletarcliente = require('./routes/deletarcliente')
+app.use('/clientes',deletarcliente)
 
-  const editarcliente = require('./routes/editarcliente')
-  app.use('/clientes',editarcliente)
+const editarcliente = require('./routes/editarcliente')
+app.use('/clientes',editarcliente)
 
-  const updatecliente = require('./routes/updatecliente')
-  app.use('/sucesso',updatecliente)
+const updatecliente = require('./routes/updatecliente')
+app.use('/sucesso',updatecliente)
 
-  // Rotas dos Pedidos
-  const pedidoroute = require('./routes/pedido')
-  app.use('/pedidos',pedidoroute)
+// Rotas dos Pedidos
+const pedidoroute = require('./routes/pedido')
+app.use('/pedidos',pedidoroute)
 
-  const RouteAddpedido = require('./routes/addpedido')
-  app.use('/pedidos/newpedido',RouteAddpedido)
+const RouteAddpedido = require('./routes/addpedido')
+app.use('/pedidos/newpedido',RouteAddpedido)
 
-  const cadastroPedido = require('./routes/cadastroPedidos')
-  app.use('/pedidos/newPedido/CadastroPedidos', cadastroPedido)
+const cadastroPedido = require('./routes/cadastroPedidos')
+app.use('/pedidos/newPedido/CadastroPedidos', cadastroPedido)
 
 // Exporta tanto o app quanto a função para startar o servidor
 function startServer(port = 3000) {
