d59c930baac338af85d8e751797d012fcce83ac7
pedrolucasods <ploliveira1805@gmail.com>
2025-11-20 15:48:07 -0400

    feat: Adiciona o tratamento do array, e salva os dados no banco!

--- src/backend/routes/cadastroPedidos.js
+++ src/backend/routes/cadastroPedidos.js
@@ -9,6 +9,36 @@ router.post('/', async function(req,res){
         let pedido = []
         pedidojson = JSON.parse(req.body.pedido)
         pedido.push(pedidojson);
+        
+
+        for(const informacoes_pedido of pedido){
+          let total = 0
+          let pedidoId = await Pedido.create({
+              cliente_id:informacoes_pedido.clienteId,
+              data: informacoes_pedido.data,
+              status:'aberto'
+            })
+            
+            for(const valor_total of informacoes_pedido.itens){
+              total += valor_total.total
+            }
+
+            for(const items_pedido of informacoes_pedido.itens){
+              ItensPedido.create({
+                id_pedido: pedidoId.id,
+                preco: items_pedido.total,
+                produto: items_pedido.produto,
+                cor: items_pedido.cor,
+                tecido: items_pedido.tecido,
+                tamanho: items_pedido.tamanho,
+                detalhes: items_pedido.detalhes,
+                quantidade: items_pedido.quantidade,
+                preco_unitario: items_pedido.precounit,
+                produto_modelo: items_pedido.modelo,
+                complemento: items_pedido.complemento
+              })
+            }
+        }
         return res.send(pedido)
     }
     
