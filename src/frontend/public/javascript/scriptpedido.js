4f6dd697ffab403750d7e26ed7374b76926ba017
pedrolucasods <ploliveira1805@gmail.com>
2025-11-22 16:33:38 -0400

    feat: Adiciona uma variavel que irá conter o id do pedido.

--- src/frontend/public/javascript/scriptpedido.js
+++ src/frontend/public/javascript/scriptpedido.js
@@ -1,4 +1,5 @@
 const newpedidobtn = document.getElementById('newpedido')
+let idDeletarPedido = null
 
 newpedidobtn.addEventListener('click', function(){
     window.location.href = 'pedidos/newpedido'
