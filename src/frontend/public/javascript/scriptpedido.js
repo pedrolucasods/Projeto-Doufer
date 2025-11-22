dc75fa6157eec98e19dc50bef42ae1c7a39f7cb1
pedrolucasods <ploliveira1805@gmail.com>
2025-11-22 16:36:02 -0400

    feat: Adiciona a função deletar Pedido, Confirmar Exclusão e fechar modal. Deletar Pedido: Nessa função irá pegar o id do pedido e em seguida irá abrir o modal para confimar a exclusão. Confirmar Exclusão: Irá mandar para a rota onde deleta o pedido e os itens. Fechar Modal:Retira o modal da tela

--- src/frontend/public/javascript/scriptpedido.js
+++ src/frontend/public/javascript/scriptpedido.js
@@ -4,3 +4,28 @@ let idDeletarPedido = null
 newpedidobtn.addEventListener('click', function(){
     window.location.href = 'pedidos/newpedido'
 })
+
+function deletarPedido(id){
+    idDeletarPedido = id
+    document.getElementById('modalConfirm').style.display = 'flex'
+}
+
+function confirmarExclusao(){
+    document.getElementById('modalConfirm').style.display = 'none'
+    fetch(`/pedidos/deletar/${idDeletarPedido}`,{
+        method: 'DELETE'
+    })
+        .then(response => response.text())
+        .then(html => {
+        document.body.innerHTML = html;
+        })
+        .catch(error => {
+        console.error('Erro ao deletar cliente:', error);
+        alert('Erro ao deletar cliente');
+        });
+}
+
+
+function fecharModal(){
+    document.getElementById('modalConfirm').style.display = 'none'
+}
