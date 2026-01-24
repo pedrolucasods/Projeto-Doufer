5c9dbf2cde36190280381e3689090ecfd95df7b0
pedrolucasods <ploliveira1805@gmail.com>
2026-01-24 13:23:42 -0400

    feat: Add Pedido controller and initial setup

---
+++ src/backend/controllers/pedido.js
@@ -0,0 +1,7 @@
+const modelPedido = require('../models/pedidos')
+const modelItensPedido = require('../models/itensPedidos')
+const modelCliente = require('../models/cliente')
+
+class Pedido{
+    
+}
