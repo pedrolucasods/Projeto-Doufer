4d68d57f6210519bc17d5f305435ae1b1e48036b
pedrolucasods <ploliveira1805@gmail.com>
2026-04-01 15:41:15 -0400

    refactor: Remove rotas não utilizadas para gerenciar medidas e pedidos, simplificando a API.

--- src/backend/routes/clienteroutes.js
+++ src/backend/routes/clienteroutes.js
@@ -14,9 +14,6 @@ router.get('/editar/:id', ClienteController.formEditar)
 // detalhes cliente
 router.get('/detalhes/:id', ClienteController.detalhes)
 
-// cadastrar medidas cliente
-router.post('/medidas/:id', ClienteController.cadastrarMedidas)
-
 // formulario cadastrar Medida
 router.get('/medidas/:id', ClienteController.formCadastrar_Medidas)
 
@@ -25,12 +22,6 @@ router.get('/medidas/listar/:id', ClienteController.listarMedidas)
 
 // editar medidas cliente
 router.get('/medidas/editar/:id', ClienteController.formEditar_medidas)
-
-// editar medidas no banco
-router.put('/medidas/editar/:id', ClienteController.editarMedidas)
-
-// limpar medidas
-router.delete('/medidas/limpar/:id', ClienteController.limparMedidas)
 
 
 module.exports = router
