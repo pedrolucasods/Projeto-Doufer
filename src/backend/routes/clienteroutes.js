ea8c82a63c3f62b9f52513e1fe387cbb96787f1e
Pedro Lucas <ploliveira1805@gmail.com>
2026-05-11 17:56:46 -0400

    Refactor: listagem de medidas do cliente (#53)
    * feat: implementa a listagem de medidas padrão de um cliente
    
    * refactor: atualiza cliente e medida controllers para melhorar o manuseio da medida
    
    * refactor: ajusta estilos e estrutura dos botões de medidas no cliente

--- src/backend/routes/clienteroutes.js
+++ src/backend/routes/clienteroutes.js
@@ -14,11 +14,6 @@ router.get('/editar/:id', ClienteController.formEditar)
 // detalhes cliente
 router.get('/detalhes/:id', ClienteController.detalhes)
 
-
-
-// listar medidas cliente
-router.get('/medidas/listar/:id', ClienteController.listarMedidas)
-
 // editar medidas cliente
 router.get('/medidas/editar/:id', ClienteController.formEditar_medidas)
 
--- src/backend/routes/medidasapis.js
+++ src/backend/routes/medidasapis.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const MedidaController = require('../controllers/medida')
 
 router.get('/clientes', MedidaController.formulario_cadastro_medidas_cliente_sob_medida)
+router.get('/clientes/Padrao/:id', MedidaController.listar_medida_padrao_cliente)
+router.get('/clientes/SobMedida/:id', MedidaController.listar_medida_sobmedida_cliente)
+
 router.post('/clientes', MedidaController.cadastrar_medida_cliente)
 router.post('/itens-pedidos', MedidaController.cadastrar_medida_itemPedido)
 
